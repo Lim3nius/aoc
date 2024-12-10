0bee651dab252d88a434667810c2ba2958fc16dd
lim3nius <lessy.mot@gmail.com>
2024-12-10 20:54:51 +0100

    2024/08 annoying one

--- 2024/08/main.py
+++ 2024/08/main.py
@@ -18,10 +18,6 @@ def process_data(data: str) -> tuple[dict[str, Any], int, int]:
     return nodes, rows, cols
 
 
-# def dist(node1: tuple[int, int], node2: tuple[int, int]) -> int:
-#     return abs(node1[0] - node2[0]) + abs(node1[1] - node2[1])
-
-
 def vec(node1: tuple[int, int], node2: tuple[int, int]) -> tuple[int, int]:
     return (node2[0] - node1[0], node2[1] - node1[1])
 
@@ -38,7 +34,9 @@ def print_matrix(nodes, rows, cols):
         print("".join(r))
 
 
-def antinodes_from(node: tuple[int, int], rows, cols, vec: tuple[int, int], infinite: bool) -> list[tuple[int, int]]:
+def antinodes_from(
+    node: tuple[int, int], rows, cols, vec: tuple[int, int], infinite: bool, key: str
+) -> list[tuple[tuple[int, int], str]]:
     antinodes = []
 
     for i in count(1):
@@ -46,7 +44,7 @@ def antinodes_from(node: tuple[int, int], rows, cols, vec: tuple[int, int], infi
         if not within_bounds(nnode, rows, cols):
             break
         else:
-            antinodes.append(nnode)
+            antinodes.append((nnode, key))
 
         if not infinite:
             break
@@ -62,32 +60,9 @@ def compute_antinodes(
     for k, antenas in nodes.items():
         for comb in combinations(antenas, 2):
             v = vec(comb[0], comb[1])
-            node1 = (comb[1][0] + v[0], comb[1][1] + v[1])
-            if within_bounds(node1, rows, cols):
-                antinodes.append((node1, k))
-            # print(f"combination {comb}, has vec: {v} and node1: {node1}")
-
-            if repetition:
-                for i in count(2):
-                    nnode = (comb[1][0] + i * v[0], comb[1][1] + i * v[1])
-                    if not within_bounds(nnode, rows, cols):
-                        break
-                    else:
-                        antinodes.append((nnode, k))
-
             v_ = (-1 * v[0], -1 * v[1])
-            node2 = (comb[0][0] + v_[0], comb[0][1] + v_[1])
-            if within_bounds(node2, rows, cols):
-                antinodes.append((node2, k))
-            # print(f"combination {comb}, has vec: {v_} and node2: {node2}")
-
-            if repetition:
-                for i in count(2):
-                    nnode = (comb[0][0] + i * v_[0], comb[0][1] + i * v_[1])
-                    if not within_bounds(nnode, rows, cols):
-                        break
-                    else:
-                        antinodes.append((nnode, k))
+            antinodes.extend(antinodes_from(comb[1], rows, cols, v, repetition, k))
+            antinodes.extend(antinodes_from(comb[0], rows, cols, v_, repetition, k))
 
     return antinodes
 
@@ -124,6 +99,8 @@ def part2(data: str) -> Any:
     print()
 
     antinodes = compute_antinodes(nodes, rows, cols, True)
+    antinodes.extend(dct_to_lst(nodes))
+
     print_matrix(antinodes, rows, cols)
 
     return len(set([t[0] for t in antinodes]))
